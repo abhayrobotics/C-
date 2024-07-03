649f720613e92c97595d66c0e52aa3f7e1165b71
Abhay kumar <abhayrobotics1908@gmail.com>
2024-07-04 00:44:58 +0530

    first program

---
+++ Basic/demo.cpp
@@ -0,0 +1,16 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int x;
+//     cin >> x
+//     cout <<"hey"<<x;
+// }
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
