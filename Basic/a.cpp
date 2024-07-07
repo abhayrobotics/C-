4c9b9505266cf3bc5b5f743c2177e376396af609
Abhay kumar <abhayrobotics1908@gmail.com>
2024-07-07 22:21:25 +0530

    initialised app

---
+++ Basic/a.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <bits/stdc++.h>
+#include  <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // variable declaration
+    int x;
+
+    // long , long long ,float, double;
+    
+    string var1;
+    // cout << "hello";
+    // cin >> var1;
+    getline(cin,var1);
+
+    cout << "hellso " << var1;
+
+    return 0;
+}
