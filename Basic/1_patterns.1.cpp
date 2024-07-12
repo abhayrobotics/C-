cf6b7443c902625f61d103fb7ac3cd4249e9a60c
Abhay kumar <abhayrobotics1908@gmail.com>
2024-07-13 01:03:37 +0530

    pattern solving

--- Basic/1_patterns.1.cpp
+++ Basic/1_patterns.1.cpp
@@ -24,6 +24,74 @@ void print2(int n ){
 
     }
 }
+
+void print7(int n){
+    for(int i=0; i<n;i++){
+        // space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        // stars
+        for(int j=0;j<2*i+1;j++){
+            cout << "*";
+        }
+        // space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<< endl;
+    }
+}
+
+void print8(int n){
+    for(int i=0;i<n;i++){
+
+        // ?SPACE
+        for(int j=0;j<i;j++ ){
+            cout<<" ";
+        }
+        // star
+        for(int j=0;j<2*(n-i)-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void print10(int n){
+    
+ for(int i=1;i<=2*n-1;i++){
+        int stars =i;
+        if(i>n) stars = 2*n-i;
+        for(int j=1;j<=stars ;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+}
+
+void print11(int n){
+    int start =1;
+    for(int i=0;i<n;i++){
+            if(i%2==0) start=1;
+            else start=0;
+        for(int j=0;j<i;j++){
+            start =1-start;
+            cout<<start<<" ";
+        }
+        cout<<endl; 
+    }
+
+    // method-2
+     for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            if((i+j)%2==0) cout<<"1 ";
+            else cout<<"0 ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int t;
     cin>> t;
@@ -31,7 +99,10 @@ int main(){
     int n;
     cin>> n;
     // print1(n);
-    print2(n);
+    // print7(n);
+    // print8(n);
+    // print10(n);
+    print11(n);
 
     }
 
