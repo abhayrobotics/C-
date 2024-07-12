c0ab071409db817ecd5d9c65bc874d11560dc320
Abhay kumar <abhayrobotics1908@gmail.com>
2024-07-13 01:52:26 +0530

    pattern

--- Basic/1_patterns.1.cpp
+++ Basic/1_patterns.1.cpp
@@ -92,6 +92,61 @@ void print11(int n){
         cout<<endl;
     }
 }
+void print12(int n){
+     for(int i=1;i<=n;i++){
+        // numbers
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        // space
+        for(int j=1;j<=2*n-2*i;j++){
+            cout<<"  ";
+        }
+
+        // numbers
+        for(int j=i;j>0;j--){
+            cout<<j<<" ";
+        }
+        
+        cout<<endl;
+    }
+}
+void print13(int n){
+    int start=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<start++<<" ";
+        }
+        cout<<endl;
+    }
+}
+void print14(int n){
+    for(int i=1;i<=n;i++){
+        for(char ch='A';ch<'A'+i;ch++){
+            cout<< ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void print15(int n){
+     for(int i=1;i<=n;i++){
+        for(char ch='A';ch<='A'+n -i;ch++){
+            cout<< ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+void print16(int n){
+    char ch='A';
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<ch<<" ";
+        }
+        ch++;
+        cout<<endl;
+    }
+}
 int main(){
     int t;
     cin>> t;
@@ -102,7 +157,7 @@ int main(){
     // print7(n);
     // print8(n);
     // print10(n);
-    print11(n);
+    print16(n);
 
     }
 
