24a261059ce1fe21f6da4697e7f3777cce3ba90d
Abhay kumar <abhayrobotics1908@gmail.com>
2024-07-21 13:14:54 +0530

    vector

--- Basic/2_stl.cpp
+++ Basic/2_stl.cpp
@@ -16,14 +16,68 @@ int main()
 
     // par array
     pair<int,int> arr[]= {{1,2},{2,4},{4,5}};
-    cout<< arr[2].second;
+    cout<< arr[2].second<<endl;
 
 
     // ! Vector
     vector <int> v;
     v.push_back(1);
-    cout<<v;
+    v.push_back(2);
+    v.emplace_back(3);
+    cout<<v[2]<<endl;
+
+    // vector using pair
+    vector <pair<int,int>> v2 ;
+    v2.push_back({2,3});
+    v2.emplace_back(4,5);
+    cout<< "v2[0].first- "<< v2[0].first<< endl;
+    // {{2,3},{4,5}}
+
+    vector <int> v3(3,100);
+    cout<<v3[1]<<endl;
+    // {100,100,100} {size ,value}
+
+    // ! acces vector
+    vector <int> v4={2,3,4,5};
+    vector <int> ::iterator x = v4.begin();
+    cout<< "*(x) - "<< *(x)<<endl;
+    
+    // normal variable
+    for(int i=0; i<v4.size();i++){
+        cout<<v4[i]<<" ";
+
+    }
+    cout<<"using normal loop"<<endl;
+
+    // short syntax
+    for(auto it:v4){
+        cout<<it<<" ";
+    }
+    cout<<"normal variable short syntax"<<endl;
+
+
+    // iterator
+    for(vector <int> ::iterator y = v4.begin();y<v4.end();y++){
+        cout<<*(y)<<" ";
+
+    }
+    cout<<"-  using iterator"<<endl;
+
+    // ! erase
+    v4.erase(v4.begin()+1);
+    // multi
+    // v4.erase(v4.begin()+1,v4.begin()+3);
+
+    // short
+    for(auto it= v4.begin();it!= v4.end();it++){
+        cout<<*(it)<<" ";
+    }
+     cout<<"using short cut iterator"<<endl;
+
+
 
 
     return 0;
 }
+
+
