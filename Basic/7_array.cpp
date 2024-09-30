0ad8358ae79980e19f11581e452913d2c90fe634
Abhay kumar <abhayrobotics1908@gmail.com>
2024-09-30 21:58:46 +0530

    array

--- Basic/7_array.cpp
+++ Basic/7_array.cpp
@@ -10,11 +10,27 @@ void largestNo(vector <int> &arr){
                 largest = arr[i];
             }
         }
-       cout<< largest;
+       cout<< largest<<endl;
 
 }
 
-
+void getSecondLargest(vector<int> &arr) {
+        // Code Here
+        int largest =arr[0];
+        int slargest = -1;
+        
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]>largest){
+                slargest = largest;
+                largest =arr[i];
+                
+            }
+            else if(arr[i]<largest && arr[i]>slargest){
+                slargest = arr[i];
+            }
+        }
+        cout<< slargest;
+}
 int main (){
     // cout<<"check";
     int n;
@@ -29,6 +45,6 @@ int main (){
     
   
     largestNo(arr);
-
+    getSecondLargest(arr);
     return 0;
 }
